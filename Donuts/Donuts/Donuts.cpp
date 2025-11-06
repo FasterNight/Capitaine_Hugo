93f95b6f8542b8cd09cf2febdaac8f106939fe50
FasterNight <147041418+FasterNight@users.noreply.github.com>
2025-11-06 16:36:46 +0100

    push save

--- Donuts/Donuts/Donuts.cpp
+++ Donuts/Donuts/Donuts.cpp
@@ -1,21 +1,51 @@
 #include "Settings.h"
 #include <iostream>
 
+#define Vert = "\e[38;2;0;255;0m"
+#define Close = "\e[0m"
+
 int main(int argc, char* argv[])
 {
-    std::cout << "\x1b[2J\n";   // Efface tous les caractères de la console 
-    
-    std::cout << "\x1b[H\n";    // Met à la position (0,0) le curseur 
-    
+    //std::cout << "\x1b[2J\n";   // Efface tous les caractères de la console 
+    //
+    //std::cout << "\x1b[H\n";    // Met à la position (0,0) le curseur 
+    //
  
-    std::cout << "\x1b[?25l\n"; // Rend invisible le curseur
-    
-    std::cout << "Hello World!\n";
-    
-    std::cout << "\x1b[?25h\n"; // Rend visible le curseur 
+    //std::cout << "\x1b[?25l\n"; // Rend invisible le curseur
+    //
+    //std::cout << "Hello World!\n";
+    //
+    //std::cout << "\x1b[?25h\n"; // Rend visible le curseur 
 
-    
-    Settings s(atoi(argv[1]), atoi(argv[2])); // Constructeur du tableau 
+    int HeightValue = 0;
+    int WidthValue = 0;
+
+    for (int i = 1; i < argc; ++i)
+    {
+        std::string arg = argv[i];
+
+        if (arg == "-h" && i + 1 < argc)
+        {
+            HeightValue = atoi(argv[i + 1]);
+        }
+        else if (arg == "-w" && i + 1 < argc)
+        {
+            WidthValue = atoi(argv[i + 1]);
+        }
+        else
+        {
+            std::cout << "|=====================|HELP|=======================|" << std::endl;
+            std::cout << "Use -h for set the height and -w for set the width !" << std::endl;
+            std::cout << "\n";
+            std::cout << Vert << "[EXEMPLE] -h 20 -w 100" << Close <<  std::endl;
+            return 0;
+        }
+    }
+
+
+
+
+    Settings s(HeightValue, WidthValue); // Constructeur du tableau 
 
     for (int i = 1; i < s.GetHeight(); i++) // Hauteur
     {
